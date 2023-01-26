daab840541da70a27c608a7ad42547564d8e7a6b
Shinichiro Sonoda <shinichiro.sonoda@fujifilm.com>
2023-01-26 18:41:22 +0900

    db add

---
+++ mems_server/db_init.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import os, sqlite3
+
+# define DB
+sql_method_1 = """
+               CREATE TABLE board(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               board_id TEXT,
+               file_path  TEXT,
+               flag TEXT,
+               date TEXT
+               )
+               """
+               
+
+db_path_1 = './db/mems_board.db'
+old_path_1 = './db/mems_board_old.db'
+
+#db_path_1 = './mems.db'
+#old_path_1 = './mems_old.db'
+
+# DB initiarize
+def db_init(db_path, old_path, sql_method):
+
+    # save old db and reset db
+    db_is_new = not os.path.exists(db_path)
+    old_is_new = not os.path.exists(old_path)
+
+    if not old_is_new:
+        os.remove(old_path)
+
+    if not db_is_new:
+        os.rename(db_path, old_path)
+
+    conn = sqlite3.connect(db_path)
+
+    # テーブル作成
+    conn.execute(sql_method)
+
+    conn.commit()
+    conn.close()
+
+
+def db_init_main():
+    db_init(db_path_1, old_path_1, sql_method_1)
+
+if __name__ == '__main__':
+    db_init_main()
+
+    
+
+
+
+
--- mems_server/server.py
+++ mems_server/server.py
@@ -1,8 +1,6 @@
-#app.py
-#from cgitb import text
-#from telnetlib import XASCII
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from flask_httpauth import HTTPBasicAuth
+import json
 
 from io import BytesIO
 import urllib
@@ -15,6 +13,12 @@ from data_load import draw_multi_graph2, get_all_sample_name,\
 import sys
 import ctypes
 import os
+
+from db_control import data_create, pick_up_data, update_data, delete_data
+from db_init import db_init_main
+
+# DBの初期化
+db_init_main()
 
 # Set title
 title = "MEMS server"
@@ -93,6 +97,81 @@ def list():
     func = request.json["func"]
     sample_list = get_all_sample_name(func)
     return jsonify({'sample_list': sample_list})
+
+
+# DB
+@app.route('/data_create', methods=['POST'])
+def f_data_create():
+    # jsonから変換
+    data = request.data.decode('utf-8')
+    board_id, file_name, flag = json2data(data)
+    print(board_id, file_name, flag)
+
+    # DBにレコードを作成
+    id = data_create(board_id, file_name, flag)
+    print('id= ',id)
+
+    # HTTPレスポンスを送信
+    return Response(response=json.dumps({"message": "{} was recieved".format(id)}), status=200)
+    
+
+@app.route('/pick_up_data', methods=['POST'])
+def f_pick_up_data():
+    # jsonから変換
+    data = request.data.decode('utf-8')
+    board_id, _, _ = json2data(data)
+    
+    # DBを読み出し   
+    data = pick_up_data(board_id)
+
+    # data取り出し
+    if data != []:
+        board_id, file_name, flag , time= data[0][1], data[0][2], data[0][3], data[0][4]
+    
+        # JSONに変換
+        data = data2json(board_id, file_name, flag, time)
+        return Response(response=data, status=200)
+    else:
+        return Response(response="None", status=200)
+
+@app.route('/update_data', methods=['POST'])
+def f_update_data():
+    # jsonから変換
+    data = request.data.decode('utf-8')
+    board_id, file_name, flag = json2data(data)
+
+    # DBにレコードを作成
+    data = update_data(board_id, file_name, flag)
+
+    # data取り出し   
+    board_id, file_name, flag , time= data[0][1], data[0][2], data[0][3], data[0][4]
+
+    # JSONに変換
+    data = data2json(board_id, file_name, flag, time)
+
+    return Response(response=time, status=200)
+
+@app.route('/delete_data', methods=['POST'])
+def f_delete_data():
+    # jsonから変換
+    data = request.data.decode('utf-8')
+    board_id, _, _ = json2data(data)
+    
+    # DBを読み出し   
+    data = delete_data(board_id)
+
+    return Response(response=data, status=200)
+
+def json2data(data):
+    data_json = json.loads(data)
+    board_id, file_name, flag = data_json['board_id'], data_json['file_name'], data_json['flag']
+    return board_id, file_name, flag
+
+def data2json(board_id, file_name, flag, time):
+    return json.dumps({"board_id":"{}".format(board_id),\
+                       "file_name":"{}".format(file_name),\
+                       "flag":"{}".format(flag),\
+                       "time":"{}".format(time)})
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
