f7cd4d1d5ddab87c343b6dbffe0c62a25aa8de97
Shinichiro Sonoda <shinichiro.sonoda@fujifilm.com>
2023-02-03 08:39:10 +0900

    merge db_server

--- mems_server/server.py
+++ mems_server/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, Response
 from flask_httpauth import HTTPBasicAuth
-import json
 
 from io import BytesIO
 import urllib
@@ -14,11 +13,14 @@ import sys
 import ctypes
 import os
 
-from db_control import data_create, pick_up_data, update_data, delete_data
-from db_init import db_init_main
+import json
 
 # DBの初期化
+from db_init import db_init_main
 db_init_main()
+
+from db_control import data_create_board, data_create_fov,\
+                       pick_up_board_id, pick_up_sample_name, pick_up_id
 
 # Set title
 title = "MEMS server"
@@ -98,80 +100,73 @@ def list():
     sample_list = get_all_sample_name(func)
     return jsonify({'sample_list': sample_list})
 
-
 # DB
-@app.route('/data_create', methods=['POST'])
-def f_data_create():
+@app.route('/data_create_board', methods=['POST'])
+def f_data_create_board():
     # jsonから変換
-    data = request.data.decode('utf-8')
-    board_id, file_name, flag = json2data(data)
-    print(board_id, file_name, flag)
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
 
     # DBにレコードを作成
-    id = data_create(board_id, file_name, flag)
-    print('id= ',id)
+    id = data_create_board(data_dict)
 
     # HTTPレスポンスを送信
-    return Response(response=json.dumps({"message": "{} was recieved".format(id)}), status=200)
-    
+    return Response(response=json.dumps({"id": id}), status=200)
 
-@app.route('/pick_up_data', methods=['POST'])
-def f_pick_up_data():
+@app.route('/data_create_fov', methods=['POST'])
+def f_data_create_fov():
     # jsonから変換
-    data = request.data.decode('utf-8')
-    board_id, _, _ = json2data(data)
-    
-    # DBを読み出し   
-    data = pick_up_data(board_id)
-
-    # data取り出し
-    if data != []:
-        board_id, file_name, flag , time= data[0][1], data[0][2], data[0][3], data[0][4]
-    
-        # JSONに変換
-        data = data2json(board_id, file_name, flag, time)
-        return Response(response=data, status=200)
-    else:
-        return Response(response="None", status=200)
-
-@app.route('/update_data', methods=['POST'])
-def f_update_data():
-    # jsonから変換
-    data = request.data.decode('utf-8')
-    board_id, file_name, flag = json2data(data)
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
 
     # DBにレコードを作成
-    data = update_data(board_id, file_name, flag)
+    id = data_create_fov(data_dict)
 
-    # data取り出し   
-    board_id, file_name, flag , time= data[0][1], data[0][2], data[0][3], data[0][4]
+    # HTTPレスポンスを送信
+    return Response(response=json.dumps({"id": id}), status=200)
+    
 
-    # JSONに変換
-    data = data2json(board_id, file_name, flag, time)
-
-    return Response(response=time, status=200)
-
-@app.route('/delete_data', methods=['POST'])
-def f_delete_data():
+@app.route('/pick_up_board_id', methods=['POST'])
+def f_pick_up_board_id():
     # jsonから変換
-    data = request.data.decode('utf-8')
-    board_id, _, _ = json2data(data)
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
+    board_id = data_dict["board_id"]
+    path = data_dict["path"]
     
     # DBを読み出し   
-    data = delete_data(board_id)
+    data = pick_up_board_id(board_id, path)
 
-    return Response(response=data, status=200)
+    return Response(response=json.dumps(data), status=200)
 
-def json2data(data):
-    data_json = json.loads(data)
-    board_id, file_name, flag = data_json['board_id'], data_json['file_name'], data_json['flag']
-    return board_id, file_name, flag
+@app.route('/pick_up_sample_name', methods=['POST'])
+def f_pick_up_sample_name():
+    # jsonから変換
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
+    sample_name = data_dict["sample_name"]
+    path = data_dict["path"]
+    
+    # DBを読み出し   
+    data = pick_up_sample_name(sample_name, path)
 
-def data2json(board_id, file_name, flag, time):
-    return json.dumps({"board_id":"{}".format(board_id),\
-                       "file_name":"{}".format(file_name),\
-                       "flag":"{}".format(flag),\
-                       "time":"{}".format(time)})
+    return Response(response=json.dumps(data), status=200)
+
+@app.route('/pick_up_id', methods=['POST'])
+def f_pick_up_id():
+    # jsonから変換
+    data_json = request.data.decode('utf-8')
+    data_dict = json.loads(data_json)
+    id = data_dict["id"]
+    path = data_dict["path"]
+    
+    # DBを読み出し   
+    data = pick_up_id(id, path)
+
+    return Response(response=json.dumps(data), status=200)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
