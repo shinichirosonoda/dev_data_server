41bab65773f3f7418ee535a256694a01d03fbac0
Shinichiro Sonoda <shinichiro.sonoda@fujifilm.com>
2023-03-03 11:08:15 +0900

    db_init renew

--- db_test/db_init.py
+++ db_test/db_init.py
@@ -1,43 +1,48 @@
-# -*- coding: utf-8 -*-
 import os, sqlite3
 
-def init_config():
-    config_data =[]
-    # define board DB
-    keys = ["sample_name", "board_id", "file_name", "mes_mode", "date"]
-    sql_method = sql_text(keys)
-    db_path = './db/mems_board.db'
-    old_path = './db/mems_borad_old.db'
-    config_data.append((sql_method, db_path, old_path))
+config_data =[]
 
-    # define fov DB 
-    keys = ["sample_name", "board_id TEXT", "fov_id", "camera_id", "file_name", "date TEXT"]
-    sql_method = sql_text(keys)
-    db_path = './db/mems_fov.db'
-    old_path = './db/mems_fov_old.db'
-    config_data.append((sql_method, db_path, old_path))
+# define DB
+sql_method_1 = """
+               CREATE TABLE board(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               sample_name TEXT,
+               board_id TEXT,
+               file_name TEXT,
+               mes_mode TEXT,
+               date TEXT
+               )
+               """
 
-    # DB setting
-    sql_method_list = [data[0] for data in config_data]
-    db_path_list = [data[1] for data in config_data]
-    old_path_list = [data[2] for data in config_data]
-    
-    return db_path_list, old_path_list, sql_method_list
 
-# sql_text
-def sql_text(keys):
-    str_text = ""
-    for text in keys[:-1]:
-        str_text += text + " TEXT,"
-    str_text += keys[-1] + " TEXT"
-    sql_method = """
-                 CREATE TABLE board(
-                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                 {}
-                 date TEXT
-                 )
-                 """.format(str_text)
-    return sql_method
+db_path_1 = './db/mems_board.db'
+old_path_1 = './db/mems_borad_old.db'
+
+config_data.append((sql_method_1, db_path_1, old_path_1))
+
+
+sql_method_2 = """
+               CREATE TABLE board(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               experiment_id INTEGER,
+               sample_name TEXT,
+               board_id TEXT,
+               fov_id TEXT,
+               camera_id TEXT,
+               file_name TEXT,
+               date TEXT
+               )
+               """
+
+db_path_2 = './db/mems_fov.db'
+old_path_2 = './db/mems_fov_old.db'
+
+config_data.append((sql_method_2, db_path_2, old_path_2))
+
+# DB setting
+sql_method_list = [data[0] for data in config_data]
+db_path_list = [data[1] for data in config_data]
+old_path_list = [data[2] for data in config_data]
 
 # DB initiarize
 def db_init(db_path, old_path, sql_method):
@@ -62,7 +67,6 @@ def db_init(db_path, old_path, sql_method):
 
 
 def db_init_main():
-    db_path_list, old_path_list, sql_method_list = init_config()
     for db_path, old_path, sql_method in zip(db_path_list, old_path_list, sql_method_list) :
         db_init(db_path, old_path, sql_method)
 
